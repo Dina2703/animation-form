f30f3b2c93d743203564f1e1c5fbf33a41017997
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-11-14 20:09:27 -0500

    main funtion and validateUser, error func

---
+++ app.js
@@ -0,0 +1,33 @@
+function animatedForm() {
+    const arrows = document.querySelectorAll('.fa-arrow-down');
+
+    arrows.forEach(arrow => {
+        arrow.addEventListener("click", () => {
+            const input = arrow.previousElementSibling;
+            const parent = arrow.parentElement;
+            const nextForm = parent.nextElementSibling;
+
+            //Check for validation
+            if(input.type === "text" && validateUser(input)) {
+                console.log("it works");
+            }
+        });
+    });
+}
+
+function validateUser(user) {
+    if(user.value.length < 6) {
+        console.log("not enough characters");
+        error('rgb(189, 87, 87)');
+    } else {
+        error('rgb(87, 189, 130)');
+        return true;
+    }
+}
+
+function error(color){
+    document.body.style.backgroundColor = color;
+}
+
+
+animatedForm();
